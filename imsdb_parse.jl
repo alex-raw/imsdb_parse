d67f1353d54aadee48dc34fffafb066d23066549
alex-raw <alexanderrauhutjs@gmail.com>
2021-12-21 00:48:38 +0100

    pre christmas tests

--- imsdb_parse.jl
+++ imsdb_parse.jl
@@ -1,21 +1,30 @@
+using HTTP
+
 struct Line
     full  ::String
     clean ::String
     onset ::Int
+    tag   ::String
 end
 
-line() = Line("", "", 0)
+line() = Line("", "", 0, "")
 
 function line(x::String)
     nohtml = replace(x, r"<.*?>" => "")
     onset = findfirst(r"\S", nohtml)
-    onset = isnothing(onset) ? 0 : onset[1]
-    clean = strip(nohtml)
 
-    Line(x, clean, onset)
+    clean = strip(nohtml)
+    onset = isnothing(onset) ? 0 : onset[1]
+    tag   = pre_tag(clean)
+
+    Line(x, clean, onset, tag)
 end
 
-function pre_tag(line::Line)::String
+struct Block
+
+end
+
+function pre_tag(line::AbstractString)::String
     default = "UNC"
     patterns = Dict(
         "SCENE" => r"^\d+\.?\s*[A-Z]+", # scene numbers? also mostly INT/EXT
@@ -25,32 +34,30 @@ function pre_tag(line::Line)::String
     )
 
     for (tag, pattern) in patterns
-        if occursin(pattern, line.clean)
+        if occursin(pattern, line)
             return tag
         end
     end
     return default
 end
 
-function parse_script(path)
+function parse_script(script)
     header = true
     prev = line()
-    # tags_used = Set(String[])
     tags_used = String[]
-    # start = r"<pre>"
-    start = r"FADE IN"
+    start = r"<pre>"
+    # start = r"FADE IN"
 
-    for (n, x) in enumerate(readlines(path))
+    for (n, x) in enumerate(script)
         # skip until first match of `start`
         if header && occursin(start, x)
             header = false; else continue
         end
 
         current = line(x)
-        tag = pre_tag(current)
 
         # remember tags per block, flush when enter new block
-        push!(tags_used, tag)
+        push!(tags_used, current.tag)
         is_deepest = prev.onset - current.onset < 0
         is_deepest && empty!(tags_used) # slow with `Set` maybe just `String[]`
 
@@ -62,9 +69,33 @@ function parse_script(path)
         # -----------------------------------------
 
         prev = current
-        join([is_deepest, tags_used, tag, current], "\t", ":\t") |> println
+        join([is_deepest,
+              tags_used,
+              current.tag,
+              current.clean], "\t", ":\t") |> println
     end
 end
 
-@time parse_script("data/Joker.html")
+create_url(x) = join(["https://imsdb.com/scripts/", replace(x, " "=>"%20"), ".html"])
+imsdb_urls(title_file) = [create_url(title) for title in readlines(title_file)]
+
+function parse_scripts(path)
+    urls = imsdb_urls(path)
+    out = []
+    for (i, url) = enumerate(urls[1:10])
+        try
+            page = split(String(HTTP.get(url)), "\n")
+            push!(out, parse_script(page))
+        catch e
+            println(e.status)
+        end
+    end
+    return out
+end
+
+script = split(String(HTTP.get(urls[3])), "\n")
+
+@time parse_scripts("data/21_12_15-imsdb_titles.txt")
+
+# @time parse_script("data/Joker.html")
 
